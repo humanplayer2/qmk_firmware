597ea3ae1c0fa994ce78e81332381922d2d12b21
humanplayer2 <rasren@erst.com>
2026-01-26 09:57:27 +0100

    add unicode for aigu

--- keyboards/humanplayer2/deerfox/rules.mk
+++ keyboards/humanplayer2/deerfox/rules.mk
@@ -1,9 +1,14 @@
 # Copyright 2025 Rasmus K. Rendsvig (@humanplayer2)
 # SPDX-License-Identifier: GPL-2.0-or-later
 
-SERIAL_DRIVER = vendor
-
+# Custom shift keys
+# https://getreuer.info/posts/keyboards/custom-shift-keys/index.html
 SRC += features/custom_shift_keys.c
 
+# 
+# https://getreuer.info/posts/keyboards/non-english/index.html
+UNICODEMAP_ENABLE = yes
+
+SERIAL_DRIVER = vendor
 POINTING_DEVICE_ENABLE = yes
 POINTING_DEVICE_DRIVER = ps2_mouse
