f243f74dfb0cb963aeb531b7ab832967a0d5f0f1
humanplayer2 <rasren@erst.com>
2026-02-09 16:04:18 +0100

    more to json

--- keyboards/humanplayer2/deerfox/config.h
+++ keyboards/humanplayer2/deerfox/config.h
@@ -1,18 +1,17 @@
-// Copyright 2025 Rasmus K. Rendsvig (@humanplayer2)
+// Copyright 2025-2026 Rasmus K. Rendsvig (@humanplayer2)
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
 
+// Side controlled by eeprom:
 #define EE_HANDS
 
-#define TAPPING_TERM 175
-#define PERMISSIVE_HOLD
+// Unicode for accent aigu e:
+#define UNICODE_SELECTED_MODES UNICODE_MODE_LINUX
 
-#define FORCE_NKRO
-
-// // Trackpoint: PS/2 in Pointing Devices
-// // Config also in keyboard.json
-// // Scrolling with KC_BTN3 works on Linux because board name ends with "Trackpoint"
+// Trackpoint: Uses PS/2 in Pointing Devices PR: github.com/qmk/qmk_firmware/pull/22532
+// Some configuration is also in keyboard.json
+// Scrolling with KC_BTN3 works on Linux because board name ends with "Trackpoint"
 #define PS2_PIO_USE_PIO1
 #define PS2_MOUSE_INIT_DELAY 500
 
@@ -29,6 +28,3 @@
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_TIME 800
 #define AUTO_MOUSE_DEBOUNCE 10 // Amount of mouse movement required to switch layers: minimum 10, default 25
-
-// Unicode for accent aigu e:
-#define UNICODE_SELECTED_MODES UNICODE_MODE_LINUX
